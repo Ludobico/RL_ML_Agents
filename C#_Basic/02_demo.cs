98ac8de8225a4623ab9fd9059af8879bc5f00c8d
Ludobico <aqs450@gmail.com>
2023-02-02 11:56:42 +0900

    c#

--- C#_Basic/02_demo.cs
+++ C#_Basic/02_demo.cs
@@ -1,10 +1,11 @@
 using System;
 class UsingDemo
 {
-    static void Main()
+    static void Main0()
     {
         Console.Write("줄 바꿈 없음");
         Console.WriteLine("줄 바꿈 포함");
+        Console.WriteLine("줄\n바꿈");
     }
 }
 
